1ad047896a4117b70c13ae18267d777a842d4590
marina-nh <herediamarinarocio@gmail.com>
2024-08-08 18:12:41 -0300

    Modified Header and index files.

---
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ExploreMenu.css'
+
+const ExploreMenu = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default ExploreMenu
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,7 +4,11 @@ import './Header.css'
 const Header = () => {
   return (
     <div className='header'>
-
+        <div className="header-contents">
+            <h2>Tus postres favoritos estan acá</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur quisquam voluptate labore incidunt. Nobis, amet rem cupiditate nam maxime debitis praesentium quia repellendus enim velit exercitationem et quam autem molestias.</p>
+            <button>Mirá nuestras opciones</button>
+        </div>
     </div>
   )
 }
