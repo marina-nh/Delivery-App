b0df3c89395b70589405661b3f4a736d5f356c73
marina-nh <herediamarinarocio@gmail.com>
2024-09-02 22:57:18 -0300

    Added assets, installed packages, changes in App. Navbar finished

---
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Sidebar.css'
+import { assets } from '../../assets/assets'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+        <div className="sidebar-options">
+            <div className="sidebaroption">
+                <img src={assets.add_icon} alt="" />
+                <p>Add Items</p>
+            </div>
+            <div className="sidebaroption">
+                <img src={assets.order_icon} alt="" />
+                <p>List Items</p>
+            </div>
+            <div className="sidebaroption">
+                <img src={assets.order_icon} alt="" />
+                <p>Orders</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
