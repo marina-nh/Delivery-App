053b81bdbdd221702189a5cc55f0941358924ca4
marina-nh <herediamarinarocio@gmail.com>
2024-09-09 21:52:25 -0300

    User Authentication

---
+++ backend/controllers/userController.js
@@ -0,0 +1,11 @@
+import userModel from "../models/userModels.js";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+import validator from "validator"
+
+// login user
+const loginUser = async (req,res) => {
+
+}
+
+// register
