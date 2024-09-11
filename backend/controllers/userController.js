51abf0a586b65e7d2e4a5501b9e0fea1e8b21c17
marina-nh <herediamarinarocio@gmail.com>
2024-09-10 21:08:43 -0300

    Created login user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,27 @@ import validator from "validator"
 
 // login user
 const loginUser = async (req,res) => {
+    const {email,password} = req.body;
+    try {
+        const user = await userModel.findOne({email});
 
+        if (!user) {
+            return res.json({success:false,message:"El usuario no existe"})
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password)
+
+        if (!isMatch) {
+            return res.json({success:false,message:"Contraseña inválida"})
+        }
+
+        const token = createToken(user._id);
+        res.json({success:true,token})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
 }
 
 const createToken = (id) => {
