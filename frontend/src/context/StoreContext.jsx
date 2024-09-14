19311d7ee2025d412964a2345f5e036e6d1be60c
marina-nh <herediamarinarocio@gmail.com>
2024-09-13 23:55:53 -0300

    Completed login & registration

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -36,6 +36,12 @@ const StoreContextProvider = (props) => {
         return totalAmount;
     }
 
+    useEffect(()=>{
+        if (localStorage.getItem("token")) {
+            setToken(localStorage.getItem("token"))
+        }
+    },[])
+    
     const contextValue = {
         food_list,
         cartItems,
