2bd0ce68d3dad7731caf5890555aa5e35761ef2a
marina-nh <herediamarinarocio@gmail.com>
2024-09-14 22:24:22 -0300

    Cart controllers

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { addToCart,removeFromCart,getCart } from "../controllers/cartController.js"
+import authMiddleware from "../middleware/auth.js";
 
 const cartRouter = express.Router();
 
-cartRouter.post("/add",addToCart)
-cartRouter.post("/remove",removeFromCart)
-cartRouter.post("/get",getCart)
+cartRouter.post("/add",authMiddleware,addToCart)
+cartRouter.post("/remove",authMiddleware,removeFromCart)
+cartRouter.post("/get",authMiddleware,getCart)
 
 export default cartRouter;
