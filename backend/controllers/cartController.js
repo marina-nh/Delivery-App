ded8a9216f6ba73a64405ba233610a8c77ce667f
marina-nh <herediamarinarocio@gmail.com>
2024-09-16 19:40:19 -0300

    Add items to user cart

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -2,7 +2,21 @@ import userModel from "../models/userModels.js"
 
 // add items to user cart
 const addToCart = async (req,res) => {
-
+    try {
+        let userData = await userModel.findOne({_id:req.body.userId});
+        let cartData = await userData.cartData;
+        if(!cartData[req.body.itemId]){
+            cartData[req.body.itemId] = 1;
+        }
+        else{
+            cartData[req.body.itemId] += 1;
+        }
+        await userModel.findByIdAndUpdate(req.body.userId,{cartData});
+        res.json({success:true,message:"Added To Cart"});
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"});
+    }
 }
 
 // remove items from user cart
