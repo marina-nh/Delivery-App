0da5103936253fc4a1508ec9344a9b241c062d01
marina-nh <herediamarinarocio@gmail.com>
2024-09-18 21:29:57 -0300

    User order function for frontend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,11 +1,45 @@
 import orderModel from "../models/orderModels.js";
 import userModel from "../models/userModels.js";
-import stripe from "stripe"
+import Stripe from "stripe"
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 //placing user order for frontend
-
 const placeOrder = async (req,res) => {
 
+    try {
+        const newOrder =new orderModel({
+            userId:req.body.userId,
+            items:req.body.items,
+            amount:req.body.amount,
+            address:req.body.address
+        })
+        await newOrder.save();
+        await userModel.findByIdAndUpdate(req.body.userId,{cartData:{}});
+
+        const line_items = req.body.items.map((item)=>({
+            price_data:{
+                currency:"inr",
+                product_data:{
+                    name:item.name
+                },
+                unit_amount:item.price*100*80
+            },
+            quantity:item.quantity
+        }))
+        line_items.push({
+            price_data:{
+                currency:"inr",
+                product_data:{
+                    name:"Delivery Charges"
+                },
+                unit_amount:2*100*80
+            },
+            quantity:1
+        })
+    } catch (error) {
+        
+    }
 }
 
 export {placeOrder}
