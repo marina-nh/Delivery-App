712300c0375917705266ca99ee15ac5e909d947d
marina-nh <herediamarinarocio@gmail.com>
2024-09-19 23:38:10 -0300

    Set data

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,17 +1,34 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import './PlaceOrder.css'
 import { StoreContext } from '../../context/StoreContext'
 
 const PlaceOrder = () => {
 
-  const { getTotalCartAmount } = useContext(StoreContext)
+  const { getTotalCartAmount,token,food_list,cartItems,url} = useContext(StoreContext)
 
+  const [data,setData] = useState({
+    firstName:"",
+    lastName:"",
+    email:"",
+    street:"",
+    city:"",
+    state:"",
+    zipcode:"",
+    country:"",
+    phone:"",
+  })
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData(data => ({...data,[name]:value}))
+  }
   return (
     <form className='place-order'>
       <div className="place-order-left">
         <p className="title">Información de envío </p>
         <div className="multi-fields">
-          <input type="text" placeholder='Nombre' />
+          <input name='firstName' onChange={onChangeHandler} value={data.firstName} type="text" placeholder='Nombre' />
           <input type="text" placeholder='Apellido' />
         </div>
         <input type="email" placeholder='Email' />
