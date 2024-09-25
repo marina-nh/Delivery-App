db14dbd9cd9f8cd36a10eaa09b6253b953529382
marina-nh <herediamarinarocio@gmail.com>
2024-09-24 23:36:24 -0300

    CompUser order page

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -71,5 +71,15 @@ const verifyOrder = async (req,res) => {
     }
 }
 
+// user orders for frontend
+const userOrders = async (req,res) => {
+    try {
+        const orders = await orderModel.find({userId:req.body.userId});
+        res.json({success:true,data:orders})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+}
 
-export {placeOrder,verifyOrder}
+export {placeOrder,verifyOrder,userOrders}
