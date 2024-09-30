5dc76acc21e05cb74ebcefa7a828835653435b5d
marina-nh <herediamarinarocio@gmail.com>
2024-09-30 17:48:06 -0300

    Completed

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 //placing user order for frontend
 const placeOrder = async (req,res) => {
-
+    //http://localhost:5174
     const frontend_url = "http://localhost:5173"
 
     try {
