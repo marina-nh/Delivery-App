509157409c6a2597162bc872648b3f35e7a6adbf
marina-nh <herediamarinarocio@gmail.com>
2024-09-30 17:40:43 -0300

    Completed

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -19,6 +19,16 @@ const Orders = ({url}) => {
     }
   }
 
+  const statusHandler = async (event,orderId) => {
+    const response = await axios.post(url+"/api/order/status",{
+      orderId,
+      status:event.target,value
+    })
+    if (response.data.success) {
+      await fetchAllOrders();
+    }
+  }
+
   useEffect(()=>{
     fetchAllOrders();
   },[])
@@ -50,7 +60,7 @@ const Orders = ({url}) => {
             </div>
             <p>Items : {order.items.length}</p>
             <p>${order.amount}</p>
-            <select>
+            <select onChange={(event)=>statusHandler(event,order._id)} value={order.status}>
               <option value="Food Processing">Food Processing</option>
               <option value="Out for delivery">Out for delivery</option>
               <option value="Delivered">Delivered</option>
